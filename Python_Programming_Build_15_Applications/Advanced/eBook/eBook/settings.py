b3421d18bad47de18b2ab530319c297507881cfb
Benjamín Aage <benjaminaage@hotmail.com>
2021-09-23 16:02:51 +0000

    Update settings.py

--- Python_Programming_Build_15_Applications/Advanced/eBook/eBook/settings.py
+++ Python_Programming_Build_15_Applications/Advanced/eBook/eBook/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p1ebyqwn8*=(z@e4d7i4g6w&a_83^lt17f-n5tolnb3a)iq438'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
