38b6eb4ea324c5b61c47f0fd5dff34567bb2e6e1
Benjamín Aage <benjaminaage@hotmail.com>
2021-12-06 17:45:42 +0000

    Add files via upload

---
+++ Video_Game_Creation_With_Python_and_Pygame/0_Basic_Pygame/Coding/5_adding_sounds.py
@@ -0,0 +1,45 @@
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Create a display surface
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 300
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Adding Sounds!")
+
+# Load sounds effects
+sound_1 = pygame.mixer.Sound('0_Basic_Pygame/Sounds/sound_1.wav')
+sound_2 = pygame.mixer.Sound('0_Basic_Pygame/Sounds/sound_2.wav')
+
+# Play the sound effects
+sound_1.play()
+pygame.time.delay(2000)  # milliseconds
+sound_2.play()
+pygame.time.delay(2000)
+
+# Change the volume of a sound effect
+sound_2.set_volume(.1)
+sound_2.play()
+
+# Load background music
+pygame.mixer.music.load('0_Basic_Pygame/Sounds/music.wav')
+
+# Play and stop the music
+# .play(Times of loop (-1 = forever), and starting time of music play) file
+pygame.mixer.music.play(-1, 0.0)
+pygame.time.delay(1000)
+sound_2.play()
+pygame.time.delay(5000)
+pygame.mixer.music.stop()
+
+# The main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# End game
+pygame.quit()
